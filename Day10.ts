c6bd32d35c50ccb8b6ddac1c2be8bbd88198efbe
Hiba-tanveer19 <tanveerhiba97@gmail.com.com>
2024-06-06 21:25:53 +0500

    Assignment Challenge

--- Day10.ts
+++ Day10.ts
@@ -1,4 +1,5 @@
-//Stages of a person's life with an If/Else chain//
+//Question 28: Stages of Life: Determine a person’s life stage with an if-else chain.//
+
 let age = 25;
 
 if (age < 2) {
@@ -15,7 +16,9 @@ if (age < 2) {
     console.log("The person is an elder!");
 }    
 
-//Array with If/Else condition//
+
+//Question 29: Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.//
+
 let faviee_fruits: string[] = ["Mango" , "Pineapple" , "Pomegranate"];
 if (faviee_fruits.includes("Mango")) {
     console.log("You really like Mango!");
@@ -23,13 +26,11 @@ if (faviee_fruits.includes("Mango")) {
 if (faviee_fruits.includes("Pomegranate")) {
     console.log("You really like Pomegranate!");
 }
-    
-// Array method //
-const numbers = [1, 2, 3, 4, 5];
-numbers.forEach(number => console.log(number * 2));
+ 
 
-// Array, Loop & If/Else condition //
-let usernames: string[] = ["admin" , "user1" , "user2" , "user3" , "user4"];
+//Question 30: Hello Admin: Greet users differently, especially 'admin'.//
+
+let username: string[] = ["admin" , "user1" , "user2" , "user3" , "user4"];
 usernames.forEach(usernames => {
     if (usernames === "admin") {
         console.log("Hello Admin, would you like to see a status report?");
